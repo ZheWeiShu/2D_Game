4942c49e4deccb3bd4e1fb206f3775290ed01ce6
ZheWeiShu <>
2019-11-02 21:01:55 +0800

    完成場景、介面與新增地板腳本

---
+++ Unity Project/Assets/程式/Ground.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float startX;
+    private float startY;
+    void Start()
+    {
+        startX = transform.position.x;
+        startY = transform.position.y;
+    }
+
+    
+    void Update()
+    {
+        if (transform.position.x <= -(startX - 2))
+        {
+            transform.position = new Vector2(startX, startY);
+        }
+        transform.Translate(transform.right * speed);
+    }
+}
